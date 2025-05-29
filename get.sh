62f3699e2aa1cbf85cc29408c8ee41b54e7b7117
Eddie Hill <550046@escg.ac.uk>
2025-05-29 09:22:39 +0100

    2600-2750

--- get.sh
+++ get.sh
@@ -3,8 +3,8 @@
 # Global variables
 baselnk="https://myrient.erista.me/files/Redump/Nintendo%20-%20Wii%20-%20NKit%20RVZ%20[zstd-19-128k]"
 
-start=2500
-count=100
+start=2600
+count=150
 
 index=0
 
